b2e47dfabe681c199228c6ba4c22ddfed90a4c91
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-27 16:47:44 +0900

    libusbのテスト。 ※find_bussesで失敗している。

--- Socket/main.cpp
+++ Socket/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include "lusb0_usb.h"
 
 int main()
 {
 	std::cout << "Socket Program" << std::endl;
-	std::cin.get();
+	
+	usb_init();
+	if(usb_find_busses() != 0)
+	{
+		std::cout << "Find Busses Failed.";
+		std::cin.get();
+		return 1;
+	}
 
+	if (usb_find_devices() != 0)
+	{
+		std::cout << "Find Devices Failed." << std::endl;
+		std::cin.get();
+		return 1;
+	}
+
+	std::cin.get();
 	return 0;
 }
