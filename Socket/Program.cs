5417b7d56fda3177941a5595ea2719ae9889d288
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-27 13:15:42 +0900

    とりあえずUSBデバイスを列挙するだけのプログラム。

--- Socket/Program.cs
+++ Socket/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
 
 namespace Socket
 {
@@ -10,7 +11,47 @@ namespace Socket
 	{
 		public static void Main(String[] args)
 		{
-			Console.WriteLine("Socket Program.");
+			var Devices = GetUSBDevices();
+			foreach(var Device in Devices)
+			{
+				Console.WriteLine(Device.DeviceID + " -> " + Device.Description);
+			}
+		}
+
+		static List<USBDeviceInfo> GetUSBDevices()
+		{
+			List<USBDeviceInfo> Devices = new List<USBDeviceInfo>();
+
+			ManagementObjectCollection Collection;
+			using (var Searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+			{
+				Collection = Searcher.Get();
+			}
+
+			foreach (var Device in Collection)
+			{
+				Devices.Add(new USBDeviceInfo(
+					(string)Device.GetPropertyValue("DeviceID"),
+					(string)Device.GetPropertyValue("PNPDeviceID"),
+					(string)Device.GetPropertyValue("Description")
+				));
+			}
+
+			Collection.Dispose();
+			return Devices;
+		}
+
+		class USBDeviceInfo
+		{
+			public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+			{
+				this.DeviceID = deviceID;
+				this.PnpDeviceID = pnpDeviceID;
+				this.Description = description;
+			}
+			public string DeviceID { get; private set; }
+			public string PnpDeviceID { get; private set; }
+			public string Description { get; private set; }
 		}
 	}
 }
